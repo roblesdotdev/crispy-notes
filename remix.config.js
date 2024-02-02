114f4b744d68370ca246a1485961d51a01e56194
roblesdotdev <httpsnip@gmail.com>
2024-02-02 16:52:39 -0300

    setup remix flat routes

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,5 @@
+import { flatRoutes } from 'remix-flat-routes'
+
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ['**/.*'],
@@ -6,4 +8,16 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
+  tailwind: true,
+  postcss: true,
+  routes: async defineRoutes => {
+    return flatRoutes('routes', defineRoutes, {
+      ignoredRouteFiles: [
+        '.*',
+        '**/*.css',
+        '**/*.test.{js,jsx,ts,tsx}',
+        '**/__*.*',
+      ],
+    })
+  },
 }
