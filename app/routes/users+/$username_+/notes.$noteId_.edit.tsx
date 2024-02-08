3c105ae86646dbd3845fea7a0f3683413ddcaea4
roblesdotdev <httpsnip@gmail.com>
2024-02-08 16:29:53 -0300

    handle form submission with conform

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,3 +1,10 @@
+import {
+  getFormProps,
+  getInputProps,
+  getTextareaProps,
+  useForm,
+} from '@conform-to/react'
+import { getZodConstraint, parseWithZod } from '@conform-to/zod'
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node'
 import {
   Form,
@@ -6,7 +13,6 @@ import {
   useActionData,
   useLoaderData,
 } from '@remix-run/react'
-import { useEffect, useId, useRef } from 'react'
 import { z } from 'zod'
 import { GeneralErrorBoundary } from '~/components/error-boundary'
 import {
@@ -18,7 +24,7 @@ import {
   TextFieldErrorMessage,
 } from '~/components/ui'
 import { db } from '~/lib/db.server'
-import { invariantResponse, useHydrated, useIsPending } from '~/lib/misc'
+import { invariantResponse, useIsPending } from '~/lib/misc'
 
 const titleMaxLength = 50
 const contentMaxLength = 200
@@ -44,19 +50,15 @@ export async function loader({ params }: LoaderFunctionArgs) {
 export async function action({ params, request }: ActionFunctionArgs) {
   const formData = await request.formData()
 
-  const result = NoteEditorSchema.safeParse({
-    title: formData.get('title'),
-    content: formData.get('content'),
+  const submission = parseWithZod(formData, {
+    schema: NoteEditorSchema,
   })
 
-  if (!result.success) {
-    return json(
-      { errors: result.error.flatten(), status: 'error' },
-      { status: 400 },
-    )
+  if (submission.status !== 'success') {
+    return submission.reply()
   }
 
-  const { title, content } = result.data
+  const { title, content } = submission.value
 
   await db.note.update({
     where: { id: params.noteId },
@@ -89,79 +91,41 @@ function ErrorList({
 
 export default function NoteEditRoute() {
   const data = useLoaderData<typeof loader>()
-  const actionData = useActionData<typeof action>()
   const isPending = useIsPending({ state: 'submitting' })
-  const isHydrated = useHydrated()
-  const formRef = useRef<HTMLFormElement>(null)
-  const formId = useId()
+  const lastResult = useActionData<typeof action>()
 
-  const fieldErrors = actionData?.errors?.fieldErrors ?? null
-  const formErrors = actionData?.errors?.formErrors ?? null
-
-  const formHasErrors = Boolean(formErrors?.length)
-  const formErrorId = formErrors ? 'form-error' : undefined
-  const titleHasErrors = Boolean(fieldErrors?.title?.length)
-  const titleErrorId = titleHasErrors ? 'title-error' : undefined
-  const contentHasErrors = Boolean(fieldErrors?.content?.length)
-  const contentErrorId = contentHasErrors ? 'content-error' : undefined
-
-  console.log('Title:', titleHasErrors)
-  console.log('Content:', contentHasErrors)
-
-  useEffect(() => {
-    const formEl = formRef.current
-    if (!formEl) return
-
-    if (actionData?.status !== 'error') return
-
-    if (formEl.matches('[aria-invalid="true"]')) {
-      formEl.focus()
-    } else {
-      const firstInvalidField = formEl.querySelector('[aria-invalid="true"]')
-      if (firstInvalidField instanceof HTMLElement) {
-        firstInvalidField.focus()
-      }
-    }
-  }, [actionData])
+  const [form, fields] = useForm({
+    lastResult,
+    constraint: getZodConstraint(NoteEditorSchema),
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: NoteEditorSchema })
+    },
+    defaultValue: {
+      title: data.note.title,
+      content: data.note.content,
+    },
+  })
 
   return (
     <div className="container h-full py-3">
       <Form
         method="POST"
         className="flex h-full flex-col"
-        noValidate={isHydrated}
-        aria-invalid={formHasErrors || undefined}
-        aria-describedby={formErrorId}
-        ref={formRef}
-        id={formId}
-        tabIndex={-1}
+        {...getFormProps(form)}
       >
         <div className="flex h-full flex-1 flex-col gap-2">
-          <TextField
-            name="title"
-            defaultValue={data.note.title}
-            isInvalid={titleHasErrors || undefined}
-            aria-describedby={titleErrorId}
-          >
+          <TextField {...getInputProps(fields.title, { type: 'text' })}>
             <Label>Title</Label>
-            <Input
-              placeholder="Note title..."
-              maxLength={titleMaxLength}
-              required
-            />
-            <ErrorList errors={fieldErrors?.title} id={titleErrorId} />
+            <Input />
+            <ErrorList errors={fields.title.errors} id={fields.title.id} />
           </TextField>
-          <TextField
-            name="content"
-            defaultValue={data.note.content}
-            isInvalid={contentHasErrors}
-            aria-describedby={contentErrorId}
-          >
-            <Label>Content</Label>
-            <TextArea maxLength={contentMaxLength} required />
-            <ErrorList errors={fieldErrors?.content} id={contentErrorId} />
+          <TextField {...getTextareaProps(fields.content)}>
+            <label htmlFor={fields.content.id}>Content</label>
+            <TextArea />
+            <ErrorList errors={fields.content.errors} id={fields.content.id} />
           </TextField>
         </div>
+
         <div className="flex items-center gap-2 self-end">
           <Button type="reset" variant="outline">
             Reset
@@ -174,7 +138,7 @@ export default function NoteEditRoute() {
             Save
           </Button>
         </div>
-        <ErrorList errors={formErrors} id={formErrorId} />
+        <ErrorList errors={form.errors} id={form.id} />
       </Form>
     </div>
   )
